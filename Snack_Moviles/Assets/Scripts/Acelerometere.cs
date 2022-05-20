dd137333f65770b2c9f300d3d867eff30da96ea1
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2022-05-20 01:24:36 -0300

    shake detector DONE

--- Snack_Moviles/Assets/Scripts/Acelerometere.cs
+++ Snack_Moviles/Assets/Scripts/Acelerometere.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Acelerometere : MonoBehaviour
 {
     public bool isFlat = true;
     public float speed;
     private Rigidbody rigid;
+    public Text txt_shake;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        txt_shake.text = "";
     }
 
     void Update()
@@ -18,5 +22,13 @@ public class Acelerometere : MonoBehaviour
          Vector3 tilt = Input.acceleration;
          tilt = Quaternion.Euler(-90, 0, 0) * tilt;
          rigid.AddForce(tilt * speed);                      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ShakeNotif"))
+        {
+            txt_shake.text = "SHAKE";
+        }
     }
 }
--- Snack_Moviles/Assets/Scripts/Shake.cs
+++ Snack_Moviles/Assets/Scripts/Shake.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class Shake : MonoBehaviour
 {
+    float accelerometerUpdateInterval = 1.0f / 60.0f;
+    public float lowPassKernelWidthInSeconds;
+    float shakeDetectionThreshold = 2.0f;
+    float lowPassFilterFactor;
+    Vector3 lowPassValue;
+
+    //private Animator anim_open;
+    //private Animator anim_open_1;
+
     public GameObject salida_1;
     public GameObject salida_2;
-    Vector3 accelerator_Dir;
     void Start()
     {
-        
+        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+        shakeDetectionThreshold *= shakeDetectionThreshold;
+        lowPassValue = Input.acceleration;
+        //anim_open = salida_1.gameObject.GetComponent<Animator>();
+        //anim_open_1 = salida_2.gameObject.GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        accelerator_Dir = Input.acceleration;
-        salida_1.gameObject.SetActive(false);
-        salida_2.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        Vector3 acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        Vector3 deltaAcceleration = acceleration - lowPassValue;
+
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            //anim_open.Play("open_anim");
+            //anim_open_1.Play("open_1_anim");
+            salida_1.gameObject.SetActive(false);
+            salida_2.gameObject.SetActive(false);
+            Debug.Log("Shake event detected at time " + Time.time);
+        }
     }
+
+
 }
